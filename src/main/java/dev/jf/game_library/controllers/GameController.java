f03b9eee9329bdb64d7de905b28aa00952c1bfc3
José Ferreira <josedsferreira@gmail.com>
2025-02-25 19:06:24 +0000

    Deployment ready

--- src/main/java/dev/jf/game_library/controllers/GameController.java
+++ src/main/java/dev/jf/game_library/controllers/GameController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import dev.jf.game_library.services.GameService;
 
 @RestController
 @RequestMapping("/api/games")
+@CrossOrigin(origins = "${frontend.url}")
 public class GameController {
 
     private final GameService service;
